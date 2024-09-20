e2fe9b477ca20f7996573a4f8a341ce16b190c2c
Joon-Klaps <joon.klaps@kuleuven.be>
2024-09-20 11:21:14 +0000

    this ps

--- install-zigzag.sh
+++ install-zigzag.sh
@@ -1,7 +1,10 @@
 git clone https://github.com/suchard-group/hdtg.git zigzag
-cd zigag
+cd zigzag
 mkdir build
 cd build
-cmake ..
-cmake ..
+cmake .. --DTBB_LIBRARYDIR=/usr/include/tbb/ -DCMAKE_PREFIX_PATH=/usr/share/cmake/Modules/
+cmake .. --DTBB_LIBRARYDIR=/usr/include/tbb/ -DCMAKE_PREFIX_PATH=/usr/share/cmake/Modules/
 make
+
+CMake Error at CMakeModules/FindTBB.cmake:89 (message):
+2.255   Required library TBB not found.
