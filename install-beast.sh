cc0ffc08c5968032cefee4a4adf14a40a680cd0f
Joon-Klaps <joonklaps@gmail.com>
2024-11-11 13:22:37 +0100

    naming could change of tar dir

--- install-beast.sh
+++ install-beast.sh
@@ -8,6 +8,10 @@ wget --quiet ${BROWSER_DOWNLOAD_URL} -O BEAST${BEAST_VERSION}.tgz
 tar -xzpf BEAST${BEAST_VERSION}.tgz
 rm BEAST${BEAST_VERSION}.tgz
 
+# Names could change across versions, but should alls start with BEAST
+extracted_dir=$(ls -d BEAST*)
+mv ${extracted_dir} BEAST${BEAST_VERSION}
+
 mv BEAST${BEAST_VERSION}/bin/* /usr/local/bin
 mv BEAST${BEAST_VERSION}/lib/* /usr/local/lib
 
